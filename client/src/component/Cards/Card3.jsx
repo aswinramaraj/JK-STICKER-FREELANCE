1010e9660922a068bc669d240b1a5de77b7f89c9
aswinramaraj <ashwinramaraj682005@gmail.com>
2025-03-18 09:20:56 +0530

    add image

--- client/src/component/Cards/Card3.jsx
+++ client/src/component/Cards/Card3.jsx
@@ -5,7 +5,7 @@ import './card_animated.css'
 function Card3() {
   return (
     <Card style={{ width: '15rem' }} className='card-border'>
-      <Card.Img variant="top" src= "https://th.bing.com/th?id=OIP.jdt8VNtyICulU4jtOgoakAHaG5&w=259&h=241&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" className='card_img'/>
+      <Card.Img variant="top" src= "sticker4.jpg" className='card_img'/>
       <Card.Body className='card-body'>
         <Card.Title className='card-name'>third</Card.Title>
         
